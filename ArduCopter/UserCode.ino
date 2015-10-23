bf339b64faae96024e7820f6c5c662bee21b8735
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-10-23 13:03:09 -0700

    Enabled USERHOOK and added a tiny amount of code

--- ArduCopter/UserCode.ino
+++ ArduCopter/UserCode.ino
@@ -1,10 +1,13 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
-
+#include "GPIO.h"
 #ifdef USERHOOK_INIT
+
 void userhook_init()
 {
     // put your initialisation code here
     // this will be called once at start-up
+    hal.gpio->pinMode(40, HAL_GPIO_OUTPUT);
+    hal.console->print("GPIO 40 is output");
 }
 #endif
 
@@ -40,5 +43,6 @@ void userhook_SlowLoop()
 void userhook_SuperSlowLoop()
 {
     // put your 1Hz code here
+    hal.gpio->write(40, LOW);
 }
 #endif
